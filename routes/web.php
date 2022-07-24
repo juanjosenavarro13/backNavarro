42cbf486819c8b5a30567483be091807526028c4
juanxo13597 <juanxo97master@gmail.com>
2022-07-24 12:14:10 +0200

    jwt 401

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+Route::get('/login', function () {
+  return response()->json(['error' => 'Unauthorized'], 401);
+})->name('login');
